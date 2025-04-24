a60e6ccd8d75b3680b31d1a9e48aec09d2df60d6
SofiChamorro <mschamorry@eafit.edu.co>
2025-04-24 07:06:14 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import steamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+st.tilte("Mi tablero de dibujo")
+with st.sidebar:
+  st.subheader("Propiedades")
+  drawing_mode = st.sidebar.selectbox(
+    "formas para dibujar",
+  ("freedraw","line","rect","circle","transform","polygon","point"),
+  )
+  stroke_width= st.slider('Selecciona el ancho', 1,30,15)
+  stroke_color= st.color_picker("color de trazo","#ffffff")
+  bg_color= '#000000000'
+
+canvas_result= st.canvas(
+  fill_color="rgba(255,165,0,0.3)", #fixed fill color with some opacity
+  stroke_width= stroke_width,
+  stroke_color=stroke_color,
+  background_color=bg_color,
+  height=300,
+  width=500,
+  drawing_mode=drawing_mode,
+  key="canvas",
+)
